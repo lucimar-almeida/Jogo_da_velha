55dc39c78a91f8ceda058165c45bea4a0544e6fc
Lucimar Almeida <lucimar.a.s123@gmail.com>
2021-06-20 21:40:00 -0400

    Andamento jogo da velha

---
+++ interface.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let square = document.querySelectorAll('.square')
+
+    square.forEach((square) => {
+    square.addEventListener('click', handleClick)        
+    });
+})
+
+function handleClick(event){
+    let square = event.target
+    let position = square.id
+
+    handleMove(position)
+    updateSquare()
+}
+
+function updateSquare(){
+    let square = document.querySelectorAll(".square")
+    square.forEach((square)=>{
+        let position = square.id
+        let symbols = board[position]
+    
+    if (symbols != ""){
+        square.innerHTML = `<div class="${symbols}"></div>`
+    }
+    
+    })
+}
